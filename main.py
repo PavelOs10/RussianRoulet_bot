0cf26b597cfacf6c875a07b2564a032276d02e91
Pavel Osipov <osiov25@gmail.com>
2024-05-10 10:50:25 +0300

    Рабочая версия, изменил текст сообщений, пропала кнопка

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
 WEAPON_CHOICE = 1
@@ -6,8 +7,9 @@ FIRE = 2
 
 def start(update, context):
     context.user_data['name'] = update.message.from_user.first_name
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привет, {context.user_data['name']}! Добро пожаловать в русскую рулетку!")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Выберите оружие:\n1. Colt Python (6 камор)\n2. Smith & Wesson Model 29 (5 камор)\n3. Taurus Judge (3 каморы)\n4. Пистолет Макарова (магазин)")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Привет, {context.user_data['name']}! Сыграем в русскую рулетку?")
+    time.sleep(1)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Выбери оружие:\n1. Colt Python (6 камор)\n2. Smith & Wesson Model 29 (5 камор)\n3. Taurus Judge (3 каморы)\n4. Пистолет Макарова (магазин)")
     return WEAPON_CHOICE
 
 def handle_choice(update, context):
@@ -26,24 +28,27 @@ def handle_choice(update, context):
         }[choice]
         random.shuffle(chambers)  # перемешиваем барабан
         context.user_data['chambers'] = chambers
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Вы выбрали {weapon_name}")
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ты выбрал {weapon_name}")
         context.bot.send_message(chat_id=update.effective_chat.id, text="Игра начинается...")
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Нажмите F для выстрела...")
+        time.sleep(1)  
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Нажми ОГОНЬ для выстрела...")
         return FIRE
     elif choice == "4":
         context.user_data['weapon_name'] = "Пистолет Макарова"
-        chambers = [0, 1]  # в пистолете Макарова одна камера с боевым патроном
+        chambers = [1]  # в пистолете Макарова один боевой патрон
         context.user_data['chambers'] = chambers
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Вы выбрали Пистолет Макарова")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Ты выбрал Пистолет Макарова")
         context.bot.send_message(chat_id=update.effective_chat.id, text="Игра начинается...")
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Нажмите F для выстрела...")
+        time.sleep(1) 
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Нажми ОГОНЬ для выстрела...")
         return FIRE
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Неверный выбор. Пожалуйста, выберите число от 1 до 4.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Неверный выбор. Выбери число от 1 до 4.")
         return WEAPON_CHOICE
 
 def handle_fire(update, context):
-    if update.message.text.upper() == "F":
+    if update.message.text.upper() == "ОГОНЬ!":
+        time.sleep(2)  # задержка 2 секунды
         chambers = context.user_data['chambers']
         name = context.user_data['name']
         weapon_name = context.user_data['weapon_name']
@@ -54,21 +59,21 @@ def handle_fire(update, context):
         if current_chamber == 1:
             update.message.reply_text("БАХ!")
             if weapon_name == "Пистолет Макарова":
-                update.message.reply_text(f"Вы проиграли, {name}! Ваш пистолет Макарова пуст.")
+                update.message.reply_text(f"Ты проиграл, {name}! Ты реально хотел играть в русскую рулетку пистолетом Макарова?")
                 return start(update, context)
             else:
-                update.message.reply_text(f"Вы проиграли, {name}!")
+                update.message.reply_text(f"Ты проиграл, {name}!")
                 return start(update, context)
         else:
             empty_chambers -= 1
-            update.message.reply_text(f"Фух, вы выжили, {name}!")
+            update.message.reply_text(f"Фух, ты выжил, {name}!")
             if empty_chambers == 0:
-                update.message.reply_text(f"В стволе остался один боевой патрон! Вы выиграли, {name}!")
+                update.message.reply_text(f"В стволе остался один боевой патрон! Ты выиграл, {name}!")
                 return start(update, context)
             else:
-                update.message.reply_text("Нажмите F для выстрела...")  # Сообщение для следующего выстрела
+                update.message.reply_text("Нажмите ОГОНЬ для выстрела...")  # Сообщение для следующего выстрела
     else:
-        update.message.reply_text("Неверный выбор. Нажмите F для выстрела...")
+        update.message.reply_text("Неверный выбор. Нажми ОГОНЬ для выстрела...")
 
 def main():
     updater = Updater(token='7004037923:AAEqUxhHSpvOEiw6Yv8GRjyuM87lnQz1iVI', use_context=True)
